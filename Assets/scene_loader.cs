6682b4ada49c012586fdb1580fb86a55dc6b10e2
RifkiArifianto <rifkiarifianto@students.amikom.ac.id>
2024-12-30 15:32:48 +0700

    project start

---
+++ Assets/scene_loader.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class scene_loader : MonoBehaviour
+{
+    public Image loadingfill;
+
+    void Start(){
+        StartCoroutine(Loading());
+    }
+
+    IEnumerator Loading() {
+       AsyncOperation loading = SceneManager.LoadSceneAsync("main");
+
+       while (!loading.isDone){
+        loadingfill.fillAmount = loading.progress/0.9f;
+        yield return null;
+       }
+    }
+}
